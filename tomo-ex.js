d7e76944e1808a007871df606c894e8f6b227662
BenSav <153027450+BenSav8@users.noreply.github.com>
2026-02-26 17:28:55 -0300

    update

--- tomo-ex.js
+++ tomo-ex.js
@@ -1,21 +1,14 @@
-let styleex = document.createElement('style');
-styleex.innerHTML = `
-    @font-face {
-        font-family: 'tomo-ex';
-        src: url('https://bensav8.github.io/tomo-ex/tomo-ex.ttf') format('truetype');
-    }
-    body, * {
-        font-family: 'tomo-ex', monospace !important;
-		font-size: 9px;
-		line-height: 1.5;
-    }
-    button, * {
-		font-size: 9px;
-		line-height: 1.5;
-    }
-    textarea, * {
-		font-size: 9px;
-		line-height: 1.5;
-    }
-`;
-document.head.appendChild(styleex);
+(function() {
+	'use strict';
+
+	const style = document.createElement('style');
+	style.textContent = `
+		@font-face {
+			font-family: _tomo;
+			src: url('https://bensav8.github.io/tomo-ex/tomo-ex.ttf') format('truetype');
+            size-adjust: 90%;
+        }
+	`;
+
+	document.documentElement.appendChild(style);
+})();
