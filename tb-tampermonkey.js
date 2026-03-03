04b8c5803db874d82ce6810cfc374d5b20553e60
BenSav <153027450+BenSav8@users.noreply.github.com>
2026-03-03 13:49:27 -0300

    reverted to old code, should work fine with tb93

--- tb-tampermonkey.js
+++ tb-tampermonkey.js
@@ -4,23 +4,29 @@
 // @version      v2
 // @description  Custom trollbox font
 // @author       BenSav
-// @match        *://www.windows93.net/trollbox*
+// @match        *://v2.windows93.net/trollbox*
+// @match        *://*windows93.net/c/programs/network/Trollbox*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-	'use strict';
-
-	const style = document.createElement('style');
-	style.textContent = `
-		@font-face {
-			font-family: _tomo;
-			src: url('https://bensav8.github.io/tomo-ex/tomo-ex.ttf') format('truetype');
-            size-adjust: 90%;
-        }
-	`;
-
-	document.documentElement.appendChild(style);
-})();
-
-
+let styleex = document.createElement('style');
+styleex.innerHTML = `
+    @font-face {
+        font-family: 'tomo-ex';
+        src: url('https://bensav8.github.io/tomo-ex/tomo-ex.ttf') format('truetype');
+    }
+    body, * {
+        font-family: 'tomo-ex', monospace !important;
+		font-size: 9px;
+		line-height: 1.5;
+    }
+    button, * {
+		font-size: 9px;
+		line-height: 1.5;
+    }
+    textarea, * {
+		font-size: 9px;
+		line-height: 1.5;
+    }
+`;
+document.head.appendChild(styleex);
